16711625304fe8852e1c95fc9b71b76dd70d5e2b
Walkmaster <repouched@gmail.com>
2022-09-19 16:35:40 +0300

    task4

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -12,3 +12,22 @@ else
 {
     Console.WriteLine($"Наибольшее число {number2}, наименьшее число {number1}");
 } */
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+int maxNumber = number1;
+
+if (maxNumber < number2)
+{
+    maxNumber = number2;
+}
+if (maxNumber < number3)
+{
+    maxNumber = number3;
+}
+Console.WriteLine($"Максимальное число {maxNumber}");
