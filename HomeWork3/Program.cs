1effb55892321616c2b32395e85d294e10a0a76d
Walkmaster <repouched@gmail.com>
2022-09-23 17:20:52 +0300

    task21

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -27,3 +27,25 @@
 // {
 //     Console.WriteLine("Введено некорректное число.");
 // }
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты двух точек в пространстве.");
+Console.WriteLine("Первая координата первой точки: ");
+int xa = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая координата первой точки: ");
+int xb = int.Parse(Console.ReadLine());
+Console.WriteLine("Третья координата первой точки: ");
+int xc = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Первая координата второй точки: ");
+int ya = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая координата второй точки: ");
+int yb = int.Parse(Console.ReadLine());
+Console.WriteLine("Третья координата второй точки: ");
+int yc = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(xa - ya,2) + Math.Pow(xb - yb,2) + Math.Pow(xc - yc,2));
+
+Console.WriteLine($"Расстояние между двумя точками в пространстве = {result:f2}");
