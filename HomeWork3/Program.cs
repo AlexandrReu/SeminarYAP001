c4fb241273e406a7e732fe509482b784dfcabfe2
Walkmaster <repouched@gmail.com>
2022-09-23 18:21:51 +0300

    task23

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -31,21 +31,32 @@
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine("Введите координаты двух точек в пространстве.");
-Console.WriteLine("Первая координата первой точки: ");
-int xa = int.Parse(Console.ReadLine());
-Console.WriteLine("Вторая координата первой точки: ");
-int xb = int.Parse(Console.ReadLine());
-Console.WriteLine("Третья координата первой точки: ");
-int xc = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите координаты двух точек в пространстве.");
+// Console.WriteLine("Первая координата первой точки: ");
+// int xa = int.Parse(Console.ReadLine());
+// Console.WriteLine("Вторая координата первой точки: ");
+// int xb = int.Parse(Console.ReadLine());
+// Console.WriteLine("Третья координата первой точки: ");
+// int xc = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Первая координата второй точки: ");
-int ya = int.Parse(Console.ReadLine());
-Console.WriteLine("Вторая координата второй точки: ");
-int yb = int.Parse(Console.ReadLine());
-Console.WriteLine("Третья координата второй точки: ");
-int yc = int.Parse(Console.ReadLine());
+// Console.WriteLine("Первая координата второй точки: ");
+// int ya = int.Parse(Console.ReadLine());
+// Console.WriteLine("Вторая координата второй точки: ");
+// int yb = int.Parse(Console.ReadLine());
+// Console.WriteLine("Третья координата второй точки: ");
+// int yc = int.Parse(Console.ReadLine());
 
-double result = Math.Sqrt(Math.Pow(xa - ya,2) + Math.Pow(xb - yb,2) + Math.Pow(xc - yc,2));
+// double result = Math.Sqrt(Math.Pow(xa - ya,2) + Math.Pow(xb - yb,2) + Math.Pow(xc - yc,2));
 
-Console.WriteLine($"Расстояние между двумя точками в пространстве = {result:f2}");
+// Console.WriteLine($"Расстояние между двумя точками в пространстве = {result:f2}");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число.");
+int userNumber1 = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= userNumber1; i++)
+{
+    Console.Write(Math.Pow(i,3) + " ");
+}
