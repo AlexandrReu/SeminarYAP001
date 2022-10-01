139a9d63308251008396e4e15664fea1881bd926
Walkmaster <repouched@gmail.com>
2022-10-01 20:11:18 +0500

    task25

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.WriteLine("Введите число А: ");
+// int firstNumb = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число B: ");
+// int secondNumb = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(GetMult(firstNumb, secondNumb));
+
+
+// double GetMult(int one, int two)
+// {
+//     double multi = Math.Pow(one, two);
+//     return multi;
+// }
