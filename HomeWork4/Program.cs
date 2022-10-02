03cfbf4b15169b178f2f7645116ad1ada0bd8609
Walkmaster <repouched@gmail.com>
2022-10-02 16:23:12 +0500

    task29

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -61,5 +61,17 @@
 // }
 
 
+//Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
 
+int[] array = GetRandomArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
 
+int[] GetRandomArray(int size)
+{
+    int[] result = new int[size];
+   for(int i = 0; i < size; i++)
+   {
+    result[i] = new Random().Next(0,100);
+   }
+   return result;
+}
