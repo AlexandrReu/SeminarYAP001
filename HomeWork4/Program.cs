d00c4c25bd415539d7605b03b512dbda46254966
Walkmaster <repouched@gmail.com>
2022-10-02 16:14:36 +0500

    task27

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -13,3 +13,53 @@
 //     double multi = Math.Pow(one, two);
 //     return multi;
 // }
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма цифр в числе {number} = {SumNumber(number)}");
+
+// int SumNumber(int userNumb)
+// {
+//     int result = 0;
+//     if(number >99999)
+// {
+//     Console.WriteLine("Введено некорректное число.");
+// }
+// else if((number > 9) && (number < 100))
+// {
+//     int temp1 = userNumb % 10;
+//     int temp2 = userNumb/10;
+//     result = temp1 + temp2;
+// }
+// else if((number > 99) && (number < 1000))
+// {
+//     int temp1 = userNumb % 10;
+//     int temp2 = userNumb/10%10;
+//     int temp3 = userNumb/100;
+//     result = temp1 + temp2 + temp3;
+// }
+// else if((number > 999) && (number < 10000))
+// {
+//     int temp1 = userNumb % 10;
+//     int temp2 = userNumb/10%10;
+//     int temp3 = userNumb/100%10;
+//     int temp4 = userNumb/1000;
+//     result = temp1 + temp2 + temp3 + temp4;
+// }
+// else if((number > 9999) && (number < 100000))
+// {
+//     int temp1 = userNumb % 10;
+//     int temp2 = userNumb/10%10;
+//     int temp3 = userNumb/100%10;
+//     int temp4 = userNumb/1000%10;
+//     int temp5 = userNumb/10000;
+//     result = temp1 + temp2 + temp3 + temp4 + temp5;
+// }
+//     return result;
+// }
+
+
+
+
