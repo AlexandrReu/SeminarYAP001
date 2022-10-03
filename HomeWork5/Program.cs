dcfc8508506a647de4e5d9d6c45457e4dadfe51c
Walkmaster <repouched@gmail.com>
2022-10-03 11:02:37 +0500

    task34task36

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// int[] array = GetArray(23, 100, 1000);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
+// Console.WriteLine($"Количество чётных чисел в массиве = {GetCount(array)}");
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] arr = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//        arr[i] = new Random().Next(minValue, maxValue);
+//     }
+//     return arr;
+// }
+
+// int GetCount(int[] arr)
+// {
+//     int count = 0;
+//     for(int j = 0; j < array.Length; j++)
+//     {
+//         if(array[j]%2 == 0)
+//         {
+//            count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array2 = GetArray(10, -100, 100);
+Console.WriteLine($"[{String.Join(", ", array2)}]");
+Console.WriteLine($"Сумма чисел на нечётных позициях в массиве = {GetSum(array2)}");
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+       arr[i] = new Random().Next(minValue, maxValue);
+    }
+    return arr;
+}
+
+int GetSum(int[] arr)
+{
+    int sum = 0;
+    for(int j = 0; j < arr.Length; j++)
+    {
+        if(j%2 != 0)
+        {
+            sum += arr[j];
+        }
+    }
+    return sum;
+}
