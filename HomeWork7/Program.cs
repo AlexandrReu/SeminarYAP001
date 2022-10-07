cffc7aa2028e530718af839d779861c5f3f33c93
Walkmaster <repouched@gmail.com>
2022-10-07 21:08:57 +0500

    task50

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -1,12 +1,59 @@
 ﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
-Console.WriteLine("Введите количесво строк: ");
-int str = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите количесво столбцов: ");
-int col = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количесво строк: ");
+// int str = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количесво столбцов: ");
+// int col = int.Parse(Console.ReadLine()!);
 
+// int[,] matr = FillArray(str, col, -100, 100);
+// PrintArray(matr);
+
+// void PrintArray(int[,] matr)
+// {
+//     for(int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int str = new Random().Next(3,7);
+int col = new Random().Next(3,7);
 int[,] matr = FillArray(str, col, -100, 100);
 PrintArray(matr);
+Console.WriteLine("Введите номер строки искомого элемента: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер колонки искомого элемента: ");
+int columns = int.Parse(Console.ReadLine()!);
 
+PrintTask50(matr);
+
+void PrintTask50(int[,] matr)
+{
+    if(rows >= matr.GetLength(0) || columns >= matr.GetLength(1))
+    {
+        Console.WriteLine("Элемента с таким индексом нет.");
+    }
+    else
+    {
+        for(int i = 0; i < matr.GetLength(0); i++)
+        {
+            for(int j = 0; j < matr.GetLength(1); j++)
+            {
+                if((rows == i) && (columns == j))
+                {
+                    Console.WriteLine($"Искомый элемент = {matr[i,j]} ");
+                }
+            }
+        }
+    }
+}
 
 
 
@@ -35,5 +82,3 @@ void PrintArray(int[,] matr)
         Console.WriteLine();
     }
 }
-
-
