c631654a904032a7707b3504ee247881db252006
Walkmaster <repouched@gmail.com>
2022-10-08 17:37:23 +0500

    task52

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -23,37 +23,63 @@
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-int str = new Random().Next(3,7);
-int col = new Random().Next(3,7);
-int[,] matr = FillArray(str, col, -100, 100);
+// int str = new Random().Next(3,7);
+// int col = new Random().Next(3,7);
+// int[,] matr = FillArray(str, col, -100, 100);
+// PrintArray(matr);
+// Console.WriteLine("Введите номер строки искомого элемента: ");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите номер колонки искомого элемента: ");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// PrintTask50(matr);
+
+// void PrintTask50(int[,] matr)
+// {
+//     if(rows >= matr.GetLength(0) || columns >= matr.GetLength(1))
+//     {
+//         Console.WriteLine("Элемента с таким индексом нет.");
+//     }
+//     else
+//     {
+//         for(int i = 0; i < matr.GetLength(0); i++)
+//         {
+//             for(int j = 0; j < matr.GetLength(1); j++)
+//             {
+//                 if((rows == i) && (columns == j))
+//                 {
+//                     Console.WriteLine($"Искомый элемент = {matr[i,j]} ");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количесво строк: ");
+int str = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количесво столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] matr = FillArray(str, col, 0, 100);
 PrintArray(matr);
-Console.WriteLine("Введите номер строки искомого элемента: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите номер колонки искомого элемента: ");
-int columns = int.Parse(Console.ReadLine()!);
+PrintTask52(matr);
 
-PrintTask50(matr);
-
-void PrintTask50(int[,] matr)
+void PrintTask52(int[,] matr)
 {
-    if(rows >= matr.GetLength(0) || columns >= matr.GetLength(1))
+    double sum = 0;
+    for(int j = 0; j < matr.GetLength(1); j++)
     {
-        Console.WriteLine("Элемента с таким индексом нет.");
-    }
-    else
-    {
-        for(int i = 0; i < matr.GetLength(0); i++)
+        sum = 0;
+        for(int i = 0; i < str; i++)
         {
-            for(int j = 0; j < matr.GetLength(1); j++)
-            {
-                if((rows == i) && (columns == j))
-                {
-                    Console.WriteLine($"Искомый элемент = {matr[i,j]} ");
-                }
-            }
+            sum += matr[i, j];
         }
+    sum = sum/matr.GetLength(0);
+    Console.WriteLine($"Среднее арифметическое элементов в столбце {j + 1} = {sum:f1}");
     }
 }
+
 
 
 
