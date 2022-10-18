1ef7c19074690d439f55b1613ceeda65558034f0
Walkmaster <repouched@gmail.com>
2022-10-18 17:25:51 +0500

    task66

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -1,12 +1,26 @@
 ﻿//Задача 64: Задайте значение N. 
 //Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
+// Console.WriteLine("Введите число N: ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// string Rec(int a, int b)
+// {
+//     if (a <= b) return Rec(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
+// Console.WriteLine(Rec(1,n));
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число N: ");
 int n = int.Parse(Console.ReadLine()!);
 
-string Rec(int a, int b)
-{
-    if (a <= b) return Rec(a + 1, b) + $"{a} ";
-    else return String.Empty;
+int GetSumRec(int m, int n)
+{    
+    if (n == m) return m;
+    else return n + GetSumRec(m, n - 1);    
 }
-Console.WriteLine(Rec(1,n));
+Console.WriteLine(GetSumRec(m, n));
