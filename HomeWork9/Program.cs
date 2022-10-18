3c13db7506c0e8e7555933a437f40804495b0312
Walkmaster <repouched@gmail.com>
2022-10-18 17:05:36 +0500

    task64

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 64: Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+string Rec(int a, int b)
+{
+    if (a <= b) return Rec(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+Console.WriteLine(Rec(1,n));
